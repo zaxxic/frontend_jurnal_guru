d9bbf5ec352dc637783a4db0a891b5f4c65910a2
zaxxic <renodzuhri@gmail.com>
2023-08-15 08:30:03 +0700

    testing selected

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -59,22 +59,23 @@
                 </div>
             </div>
 
-            <div class="col-md-6 d-flex align-items-stretch">
-            <div class="card w-100 position-relative overflow-hidden mb-7 mb-lg-0">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <h5 class="card-title fw-semibold mb-4">Data Kehadiran Siswa</h5>
+            <div class="col-md-8 d-flex align-items-stretch">
+                <div class="card w-100 position-relative overflow-hidden mb-7 mb-lg-0">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between">
+                            <h5 class="card-title fw-semibold mb-4">Data Kehadiran Siswa</h5>
 
-                        <button class="btn mb-5 waves-effect waves-light btn btn-primary " data-bs-toggle="modal" data-bs-target="#samedata-modal">
-                            Lihat Detail
-                        </button>
+                            <button class="btn mb-5 waves-effect waves-light btn btn-primary " data-bs-toggle="modal"
+                                data-bs-target="#samedata-modal">
+                                Lihat Detail
+                            </button>
+                        </div>
+                        <div id="chart-pie-simple"></div>
+
                     </div>
-                    <div id="chart-pie-simple"></div>
 
                 </div>
-
             </div>
-        </div>
         </div>
     </div>
 
@@ -83,7 +84,7 @@
             <div class="modal-content">
                 <div class="modal-header d-flex align-items-center">
                     <h4 class="modal-title" id="myModalLabel">
-                        Isi Jurnal /
+                        Jurnal /
                     </h4>
                     <div class=" bg-light-primary badge h-75 ms-2">
                         <span class="note-date fs-2 text-primary" style="float: right;">X-RPL</span>
@@ -91,9 +92,9 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="col-12">
-                    <div class="card w-100 position-relative overflow-hidden mb-0">
-                        <div class="card-body p-4">
-                            <form id="form-create">
+                    <form id="form-create">
+                        <div class="card w-100 position-relative overflow-hidden mb-0">
+                            <div class="card-body p-4">
                                 <input type="hidden" id="user-id" name="user_id">
                                 <div class="row">
                                     <div class="col-lg-6">
@@ -145,10 +146,11 @@
                                     </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Save</button>
-                                <button type="button"  data-dismiss="modal" class="btn btn-light-danger text-danger">Cancel</button>
-                            </form>
+                                <button type="button" data-dismiss="modal" class="btn btn-light-danger text-danger"
+                                    data-bs-dismiss="modal">Cancel</button>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
             <!-- /.modal-content -->
@@ -267,6 +269,40 @@
                     $('#data').html('')
                     if (response.data.length > 0) {
                         $.each(response.data, function(index, item) {
+
+
+                            var sick = (item.journal && item.journal.sick) ? item.journal.sick.join(
+                                ', ') : '';
+                            console.log("ini yang sakit" + sick);
+
+                            var permission = (item.journal && item.journal.permission) ? item.journal
+                                .permission.join(
+                                    ', ') : '';
+
+                            var alpha = (item.journal && item.journal.alpha) ? item.journal.alpha.join(
+                                ', ') : '';
+                            console.log("ini yang alpha " + alpha);
+
+
+
+
+
+
+
+
+                            var buttonText = item.journal ?
+                                '<button class="btn mb-1 waves-effect waves-light btn-sm btn-outline-info btn-update" id="btn-create-' +
+                                item.classroom.id + '" data-id="' + item.classroom.id +
+                                '" data-permission="' + permission +
+                                '" data-sick="' + sick +
+                                '" data-alpha="' + alpha +
+                                '" data-description="' + item.journal.description +
+                                '" data-class="' + item.classroom.name +
+                                '" data-user-id="' + item.id + '">Edit Jurnal</button>' :
+
+                                '<button class="btn mb-1 waves-effect waves-light btn-sm btn-outline-info btn-create" id="btn-create-' +
+                                item.classroom.id + '" data-id="' + item.classroom.id +
+                                '" data-user-id="' + item.id + '">Isi Jurnal</button>';
                             const firstLessonHour = item.lesson_hour[0].lesson_hour;
                             const lastLessonHour = item.lesson_hour[item.lesson_hour.length - 1]
                                 .lesson_hour;
@@ -286,11 +322,9 @@
                                     <span class="timeline-badge-border d-block flex-shrink-0"></span>
                                 </div>
                                 <div class="timeline-desc fs-3 text-dark mt-n1">${item.classroom.name} / ${item.lesson.name} <div>
-                                     <button
-                                            class=" btn mb-1 waves-effect waves-light btn-sm btn-outline-info btn-create"
-                                            id="btn-create-${item.classroom.id}" data-id="${item.classroom.id}" data-user-id="${item.id}">
-                                            Isi Jurnal 
-                                        </button> </div>
+                                     
+                                            ${buttonText} 
+                                         </div>
 
                                         
                                         
@@ -301,14 +335,23 @@
                         })
 
 
-                        $('.btn-create').click(function() {
+                        $('.btn-update').click(function() {
                             var id = $(this).data('id');
                             var userId = $(this).data('user-id');
-                            $('#form-create').data('id', id); // Simpan id ke dalam form data
-                            $('#user-id').val(userId); // Isi nilai userId ke input tersembunyi
-                            console.log(id);
-                            console.log(userId);
+                            var alpha = $(this).data('alpha');
+                            var description = $(this).data(
+                            'description'); // Asumsi: atribut data-description ada pada tombol
+                            var sick = $(this).data('sick');
+                            var permission = $(this).data('permission');
+
+
+                            var ar_alpha = alpha.split(', ');
+                            var ar_sick = sick.split(', ');
+                            var ar_permission = permission.split(', ');
+
                             $('#form-create').data('id', id);
+                            $('#user-id').val(userId);
+                            $('#description').val(description);
 
                             $.ajax({
                                 url: "{{ config('app.api_url') }}/student-classrooms/" + id,
@@ -316,7 +359,67 @@
                                 dataType: 'JSON',
                                 success: function(response) {
                                     var detailData = response.data;
-                                    console.log(detailData);
+
+                                    // Fungsi untuk mengisi elemen select dengan opsi siswa dan memilih nilai default
+                                    function populateSelectAndSetDefault(selectId,
+                                        siswaData, defaultValues) {
+                                        var select = $('#' + selectId);
+                                        select.empty();
+
+                                        $.each(siswaData, function(index, item) {
+                                            var option = '<option value="' + item
+                                                .student.id + '">' + item.student
+                                                .name + '</option>';
+                                            select.append(option);
+                                        });
+
+                                        // Menetapkan nilai-nilai default sebagai opsi terpilih
+                                        select.val(defaultValues).trigger('change');
+                                    }
+
+                                    // Mengisi elemen select untuk kategori siswa dan memilih nilai default
+                                    populateSelectAndSetDefault('select-siswa-alpha',
+                                        detailData, ar_alpha);
+                                    populateSelectAndSetDefault('select-siswa-izin',
+                                        detailData, ar_permission);
+                                    populateSelectAndSetDefault('select-siswa-sakit',
+                                        detailData, ar_sick);
+
+
+
+                                    // Tampilkan modal "create"
+                                    $('#modal-create').modal('show');
+                                },
+                                error: function(error) {
+                                    console.error("Error fetching student data:", error);
+                                }
+                            });
+                        });
+
+
+
+                        // Tangkap peristiwa pemilihan elemen select
+                        $('#select-siswa-alpha, #select-siswa-izin, #select-siswa-sakit').change(function() {
+                            var selectedValue = $(this).val();
+                            console.log('Selected Value:', selectedValue);
+                            // Lakukan sesuatu dengan nilai yang dipilih, misalnya, kirimkan melalui Ajax atau tampilkan di tempat lain.
+                        });
+
+
+
+
+                        $('.btn-create').click(function() {
+                            var id = $(this).data('id');
+                            var userId = $(this).data('user-id');
+                            $('#form-create').data('id', id);
+                            $('#user-id').val(userId);
+
+                            $.ajax({
+                                url: "{{ config('app.api_url') }}/student-classrooms/" + id,
+                                type: 'GET',
+                                dataType: 'JSON',
+                                success: function(response) {
+                                    var detailData = response.data;
 
                                     // Fungsi untuk mengisi elemen select dengan opsi siswa
                                     function populateSelect(selectId, siswaData) {
@@ -333,7 +436,7 @@
 
                                     // Akses data siswa dari relasi studentClassrooms
                                     var siswaData = detailData;
-                                    console.log(siswaData);
+
 
                                     // Mengisi elemen select untuk kategori siswa
                                     populateSelect('select-siswa-alpha', siswaData);
